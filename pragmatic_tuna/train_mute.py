bca9c840c8fb2bebd832ca3ae3c42763b2380316
Jon Gauthier <jon@gauthiers.net>
2016-11-10 20:27:00 -0800

    get simple bag model actually training

--- pragmatic_tuna/train_mute.py
+++ pragmatic_tuna/train_mute.py
@@ -11,7 +11,7 @@ from pragmatic_tuna.reinforce import reinforce_episodic_gradients
 
 
 Model = namedtuple("Model", [# inputs for computation 1: action scores
-                             "items", "utterance",
+                             "inputs",
                              # inputs for computation 2: REINFORCE gradient
                              "action", "reward",
                              # outputs for computation 1: scores
@@ -42,8 +42,9 @@ def build_model(env, item_repr_dim=50, utterance_repr_dim=50):
         utterance_repr = layers.fully_connected(utterance_repr, item_repr_dim,
                                                 tf.tanh)
 
-        scores = tf.squeeze(tf.matmul(item_reprs, tf.transpose(utterance_repr)))
+        scores = tf.matmul(item_reprs, tf.transpose(utterance_repr))
 
+    scores = tf.squeeze(scores)
     probs = tf.nn.softmax(scores)
 
     ###########
@@ -58,20 +59,28 @@ def build_model(env, item_repr_dim=50, utterance_repr_dim=50):
     rewards = reward
     gradients = reinforce_episodic_gradients(scores, actions, reward)
 
-    return Model(items, utterance,
+
+    inputs = (inputs,) if env.bag else (items, utterance)
+    return Model(inputs,
                  action, reward,
                  scores, probs,
                  gradients)
 
 
 def run_trial(model, train_op, env, sess):
-    items, utterance = env.reset()
+    inputs = env.reset()
 
+    partial_feeds = [model.action, model.reward]
+    partial_feeds += list(model.inputs)
     partial = sess.partial_run_setup([model.probs, train_op],
-                                     [model.items, model.utterance,
-                                      model.action, model.reward])
-    probs = sess.partial_run(partial, model.probs,
-                             {model.items: items, model.utterance: utterance})
+                                     partial_feeds)
+
+    if env.bag:
+        prob_feeds = {model.inputs[0]: inputs}
+    else:
+        prob_feeds = {model.inputs[0]: inputs[0],
+                      model.inputs[1]: inputs[1]}
+    probs = sess.partial_run(partial, model.probs, prob_feeds)
 
     # Sample action from probability distribution.
     choice = np.random.choice(env.domain_size, p=probs)
@@ -95,8 +104,12 @@ def build_train_graph(model, env, args):
     return train_op, global_step
 
 
+def analyze_weights(sess, env):
+    print([x.name for x in tf.trainable_variables()])
+
+
 def train(args):
-    env = TUNAEnv(args.corpus_path, args.corpus_selection)
+    env = TUNAEnv(args.corpus_path, args.corpus_selection, bag=args.bag_env)
     model = build_model(env, args.item_repr_dim, args.utterance_repr_dim)
     train_op, global_step = build_train_graph(model, env, args)
 
@@ -109,11 +122,15 @@ def train(args):
 
             run_trial(model, train_op, env, sess)
 
+        if args.analyze_weights:
+            analyze_weights(sess)
+
 
 if __name__ == "__main__":
     p = ArgumentParser()
 
     p.add_argument("--logdir", default="/tmp/tuna")
+    p.add_argument("--analyze_weights", default=False, action="store_true")
 
     p.add_argument("--corpus_path", required=True)
     p.add_argument("--corpus_selection", default="furniture")
