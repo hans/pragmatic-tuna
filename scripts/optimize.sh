d4668f5076a0656c2065fb0b15343f86aff7c612
Sebastian Schuster <sebschu@gmail.com>
2016-12-03 12:02:24 -0800

    fix optimize.sh

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -12,7 +12,7 @@ do
                 echo "Max rejections: $MR"
                 echo "Number of listener samples: $NS"
                 echo "Speaker model $SM"
-                rm -rf /tmp/tuna && python pragmatic_tuna/models/lot_rsa.py --corpus_path data/spatial_super_simple.json --atom_attribute shape --learning_method xent --num_listener_samples $NS --num_trials 2 --learning_rate $LR  --gold_path data/spatial_super_simple_gold.json --num_runs 10 --max_rejections_after_trial $MR --speaker_model "window" | grep "Average accuracy" 
+                rm -rf /tmp/tuna && python pragmatic_tuna/models/lot_rsa.py --corpus_path data/spatial_super_simple.json --atom_attribute shape --learning_method xent --num_listener_samples $NS --num_trials 2 --learning_rate $LR  --gold_path data/spatial_super_simple_gold.json --num_runs 10 --max_rejections_after_trial $MR --speaker_model $SM | grep "Average accuracy" 
             done
         done
     done
