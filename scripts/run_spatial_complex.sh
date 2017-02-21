ac9727890db1470a1f3aaa7ff4bc96c82e766d8f
Jon Gauthier <jon@gauthiers.net>
2017-02-21 13:56:54 -0800

    Why not stage these run scripts for posterity

---
+++ scripts/run_spatial_complex.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/zsh
+
+source activate jon-common
+export PYTHONPATH=.
+
+rm -rf /tmp/tuna && python pragmatic_tuna/scripts/lot_rsa.py --corpus_path data/spatial_complex.json --gold_path data/spatial_complex_gold.json --debug \
+     --fn_selection spatial_simple --max_timesteps 2 --atom_attribute type \
+     --learning_rate 0.01 --dream --num_listener_samples 128 --max_rejections_after_trial 4 --embedding_dim 8 \
+     --num_runs 1 --num_trials 26 --speaker_model sequence
+
+ # 2>&1 | less -R
+
+# --gold_path data/exclusivity_simple_gold.json \
