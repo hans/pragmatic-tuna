cfff765ce8afee0b26937a68f1528d7a41ba2ae0
Jon Gauthier <jon@gauthiers.net>
2017-02-21 14:44:31 -0800

    Fix max_timesteps for run_spatial_complex

--- scripts/run_spatial_complex.sh
+++ scripts/run_spatial_complex.sh
@@ -4,7 +4,7 @@ source activate jon-common
 export PYTHONPATH=.
 
 rm -rf /tmp/tuna && python pragmatic_tuna/scripts/lot_rsa.py --corpus_path data/spatial_complex.json --gold_path data/spatial_complex_gold.json --debug \
-     --fn_selection spatial_simple --max_timesteps 2 --atom_attribute type \
+     --fn_selection spatial_simple --max_timesteps 4 --atom_attribute type \
      --learning_rate 0.01 --dream --num_listener_samples 128 --max_rejections_after_trial 4 --embedding_dim 8 \
      --num_runs 1 --num_trials 26 --speaker_model sequence
 
