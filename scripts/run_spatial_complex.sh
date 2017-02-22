16fdbcf8ebd7383a6806565e65e07e034f42ca92
Jon Gauthier <jon@gauthiers.net>
2017-02-22 10:07:11 -0800

    Return batch sampling to spatial_complex

--- scripts/run_spatial_complex.sh
+++ scripts/run_spatial_complex.sh
@@ -4,9 +4,9 @@ source activate jon-common
 export PYTHONPATH=.
 
 rm -rf /tmp/tuna && python pragmatic_tuna/scripts/lot_rsa.py --corpus_path data/spatial_complex.json --gold_path data/spatial_complex_gold.json --debug \
-     --fn_selection spatial_simple --max_timesteps 4 --atom_attribute type \
-     --learning_rate 0.01 --dream --num_listener_samples 128 --max_rejections_after_trial 4 --embedding_dim 8 \
-     --num_runs 1 --num_trials 26 --speaker_model sequence
+     --fn_selection spatial_complex --max_timesteps 4 --atom_attribute type --dream --dream_samples 10 \
+     --learning_rate 0.01 --num_listener_samples 128 --max_rejections_after_trial 4 --embedding_dim 8 \
+     --num_runs 5 --num_trials 6 --speaker_model sequence
 
  # 2>&1 | less -R
 
