f633e4b9f6235ed7937b5aa85a7e4d9863278a46
Jon Gauthier <jon@gauthiers.net>
2017-03-30 14:29:44 -0700

    Move training graph construction to vg script; add Supervisor to support long training runs

--- pragmatic_tuna/models/ranking_listener.py
+++ pragmatic_tuna/models/ranking_listener.py
@@ -178,11 +178,9 @@ class BoWRankingListener(RankingListenerModel):
         margin_diffs = tf.maximum(0., margin + neg_scores - pos_scores)
         margin_diffs = tf.reduce_sum(margin_diffs, axis=1)
         self.loss = tf.reduce_mean(margin_diffs)
+        tf.summary.scalar("loss", self.loss)
 
-        ######### Training.
-        self.learning_rate = tf.Variable(0.001, name="learning_rate")
-        opt = tf.train.MomentumOptimizer(self.learning_rate, 0.9)#AdagradOptimizer(self.learning_rate)
-        self._train_op = opt.minimize(self.loss)
+        self.train_op = None
 
     def rank(self, words_batch, candidates_batch):
         words_batch, candidates_batch, lengths, num_candidates = \
@@ -199,8 +197,7 @@ class BoWRankingListener(RankingListenerModel):
                   for scores_i, num_candidates_i in zip(scores, num_candidates)]
         return scores
 
-    def observe(self, words_batch, true_referents_batch, false_referents_batch,
-                learning_rate=None):
+    def observe(self, words_batch, true_referents_batch, false_referents_batch):
         words_batch, false_referents_batch, lengths, num_false_referents = \
                 self._prepare_batch(words_batch, false_referents_batch)
 
@@ -212,9 +209,7 @@ class BoWRankingListener(RankingListenerModel):
         feed = {self.words[t]: words_batch[t] for t in range(self.max_timesteps)}
         feed[self.candidates] = candidates_batch
         feed[self.lengths] = lengths
-        if learning_rate is not None:
-            feed[self.learning_rate] = learning_rate
 
         sess = tf.get_default_session()
-        _, loss = sess.run((self._train_op, self.loss), feed)
+        _, loss = sess.run((self.train_op, self.loss), feed)
         return loss
--- pragmatic_tuna/scripts/vg_rsa.py
+++ pragmatic_tuna/scripts/vg_rsa.py
@@ -8,7 +8,6 @@ from pragmatic_tuna.environments.vg import VGEnv
 from pragmatic_tuna.models.ranking_listener import BoWRankingListener
 
 
-LEARNING_RATE = 0.005
 def run_trial(batch, listener_model, speaker_model):
     utterances, pos_candidates, neg_candidates = batch
 
@@ -32,16 +31,13 @@ def run_trial(batch, listener_model, speaker_model):
         successes.append(success)
 
     # Observe.
-    global LEARNING_RATE
-    loss = listener_model.observe(utterances, pos_candidates, neg_candidates, learning_rate=LEARNING_RATE)
-    if loss < 2.0 and LEARNING_RATE == 0.005:
-        pass#tqdm.write("========== Cutting learning rate.")
-        #LEARNING_RATE = 0.0005
+    loss = listener_model.observe(utterances, pos_candidates, neg_candidates)
 
     pct_success = np.mean(successes)
     tqdm.write("%5f\t%.2f" % (loss, pct_success * 100))
 
     return results, loss, pct_success
+
 
 def main(args):
     env = VGEnv(args.corpus_path)
@@ -49,43 +45,50 @@ def main(args):
     listener_model = BoWRankingListener(env, max_negative_samples=args.negative_samples)
     speaker_model = None
 
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
+    global_step = tf.Variable(0, name="global_step")
+    opt = tf.train.MomentumOptimizer(args.learning_rate, 0.9)
+    train_op = opt.minimize(listener_model.loss, global_step=global_step)
+    listener_model.train_op = train_op
 
-    with sess.as_default():
-        losses, pct_successes = [], []
-        for i in trange(args.n_iters):
-            batch = env.get_batch("train", batch_size=args.batch_size,
-                                  negative_samples=args.negative_samples)
-            predictions, loss, pct_success = run_trial(batch, listener_model, speaker_model)
+    sv = tf.train.Supervisor(logdir=args.logdir, global_step=global_step)
 
-            losses.append(loss)
-            pct_successes.append(pct_success)
+    with sv.managed_session() as sess:
+        with sess.as_default():
+            losses, pct_successes = [], []
+            for i in trange(args.n_iters):
+                batch = env.get_batch("train", batch_size=args.batch_size,
+                                    negative_samples=args.negative_samples)
+                predictions, loss, pct_success = run_trial(batch, listener_model, speaker_model)
 
-            if i == args.n_iters - 1:
-                # Debug: print utterances
-                correct, false = [], []
-                b_utt, b_pos, b_neg = batch
-                for utterance, pos_cands, neg_cands, prediction in zip(b_utt, b_pos, b_neg, predictions):
-                    utterance = " ".join([env.vocab[idx] for idx in utterance])
+                losses.append(loss)
+                pct_successes.append(pct_success)
 
-                    dest = correct if prediction == pos_cands[0] else false
-                    dest.append("%30s\t%s" % (utterance, " ".join([env.graph_vocab[idx] for idx in prediction])))
+                if i == args.n_iters - 1:
+                    # Debug: print utterances
+                    correct, false = [], []
+                    b_utt, b_pos, b_neg = batch
+                    for utterance, pos_cands, neg_cands, prediction in zip(b_utt, b_pos, b_neg, predictions):
+                        utterance = " ".join([env.vocab[idx] for idx in utterance])
 
-                print("=========== Correct:")
-                print("\n".join(correct))
+                        dest = correct if prediction == pos_cands[0] else false
+                        dest.append("%30s\t%s" % (utterance, " ".join([env.graph_vocab[idx] for idx in prediction])))
 
-                print("\n========== False:")
-                print("\n".join(false))
+                    print("=========== Correct:")
+                    print("\n".join(correct))
+
+                    print("\n========== False:")
+                    print("\n".join(false))
 
 
 if __name__ == '__main__':
     p = ArgumentParser()
 
+    p.add_argument("--logdir", default="/tmp/vg")
     p.add_argument("--corpus_path")
 
     p.add_argument("--n_iters", type=int, default=50)
     p.add_argument("--batch_size", type=int, default=64)
     p.add_argument("--negative_samples", type=int, default=5)
+    p.add_argument("--learning_rate", type=float, default=0.001)
 
     main(p.parse_args())
