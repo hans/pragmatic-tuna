6d1989eb48b94d94ed99973ee93880d34ba983e3
Jon Gauthier <jon@gauthiers.net>
2017-04-07 21:23:34 -0700

    Stage some of the sweep infra for posterity

---
+++ scripts/vg_search/002/template.sh
@@ -0,0 +1,12 @@
+#!/usr/kerberos/bin/zsh
+
+source ~/anaconda/bin/activate jon-common
+cd ~/scr/projects/pragmatic-tuna
+export PYTHONPATH=.
+
+logdir=$VG_LOG_DIR/<expnum:>
+mkdir -p $logdir
+
+python pragmatic_tuna/scripts/vg_rsa.py --corpus_path data/vg_processed.pkl --n_iters 20000 --batch_size <batch_size:E:64,128> \
+    --listener_learning_rate <llr:L:0.0001,0.001> --speaker_lr_factor <slr:L:50,500> --dropout_keep_prob <drop:E:0.8,1.0> \
+    --logdir $logdir > ${logdir}/stdout 2> ${logdir}/stderr
