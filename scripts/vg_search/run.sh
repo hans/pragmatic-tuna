02af82647c3ecb1694cea8330ee8761fbfc0e253
Jon Gauthier <jon@gauthiers.net>
2017-04-07 21:25:53 -0700

    Fix VG_LOG_DIR

--- scripts/vg_search/run.sh
+++ scripts/vg_search/run.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 host=`hostname | tr '.' '\n' | head -n1`
-export VG_LOG_DIR=/$host/scr1/`whoami`/vg
+export VG_LOG_DIR=/$host/scr1/`whoami`/vg/$SWEEP
 mkdir -p $VG_LOG_DIR
 
 echo "Setting up for sweep: " $SWEEP
