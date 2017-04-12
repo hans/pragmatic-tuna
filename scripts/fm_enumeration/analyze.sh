e7ff18dc3bab22481a02eeef6284a65d5c1f48c5
Jon Gauthier <jon@gauthiers.net>
2017-04-12 14:44:58 -0700

    Fix analysis: use only last 5 trials

--- scripts/fm_enumeration/analyze.sh
+++ scripts/fm_enumeration/analyze.sh
@@ -1,3 +1,3 @@
-for x in `ls results/*.out`; do grep 'L_ADV' $x > $x.dream; done
+for x in `ls results/*.out`; do grep 'L_ADV' $x | tail -n 5 > $x.dream; done
 for x in `ls results/*.dream`; do fname=`echo $x | sed "s/out\.dream/dev_success/"`; awk -F "\t" '{split($3,a,":"); sum += a[2]; n+= 1} END {print sum / n}' < $x > $fname; done
 for x in `ls results/*.dream`; do fname=`echo $x | sed "s/out\.dream/pt_success/"`; awk -F "\t" '{split($7,a,":"); sum += a[2]; n+= 1} END {print sum / n}' < $x > $fname; done
