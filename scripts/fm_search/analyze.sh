7e1160c73eae1dd4932663484863af33fcc51c77
Jon Gauthier <jon@gauthiers.net>
2017-04-16 13:55:58 -0700

    Sort by advfm, not fm performance

--- scripts/fm_search/analyze.sh
+++ scripts/fm_search/analyze.sh
@@ -2,11 +2,12 @@
 
 SWEEP_DIR=/jagupard13/scr1/jgauthie/vg_dream/005_fm
 
+echo "K\tADVFM\tFM\tPT\tID"
 for x in `find $SWEEP_DIR -type d`; do
     k=`awk -F ':' '/fast_mapping_k/ {gsub(",","",$2); print $2}' < $x/params`
     fm_avg=`grep ADVFM $x/stdout | tail -n5 | awk '{sum += $6; n += 1} END {print sum / n}'`
     advfm_avg=`grep ADVFM $x/stdout | tail -n5 | awk '{sum += $8; n += 1} END {print sum / n}'`
     dev_avg=`grep ADVFM $x/stdout | tail -n5 | awk '{sum += $10; n += 1} END {print sum / n}'`
     name=`basename $x`
-    echo "$k\t$fm_avg\t$advfm_avg\t$dev_avg\t$name"
-done | sort -n
+    echo "$k\t$advfm_avg\t$fm_avg\t$dev_avg\t$name"
+done | grep -v nan | sort -n
