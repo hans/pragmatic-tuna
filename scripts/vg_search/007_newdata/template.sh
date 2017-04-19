3236e675548e0fc8b3f31faff1b3f2169b499407
Jon Gauthier <jon@gauthiers.net>
2017-04-18 22:35:56 -0700

    Use the dedup data

--- scripts/vg_search/007_newdata/template.sh
+++ scripts/vg_search/007_newdata/template.sh
@@ -7,7 +7,7 @@ export PYTHONPATH=.
 logdir=$VG_LOG_DIR/<expnum:>
 mkdir -p $logdir
 
-python pragmatic_tuna/scripts/vg_rsa.py --corpus_path data/vg_processed_2_2.pkl --n_iters 20000 --batch_size <batch_size:E:64> \
+python pragmatic_tuna/scripts/vg_rsa.py --corpus_path data/vg_processed_2_2.dedup.pkl --n_iters 20000 --batch_size <batch_size:E:64> \
     --listener_learning_rate <llr:L:0.0001,0.001> --speaker_lr_factor <slr:L:50,500> --dropout_keep_prob <drop:E:0.8,1.0> \
     --speaker_hidden_dim <shid:LI:384,1024> \
     --logdir $logdir > ${logdir}/stdout 2> ${logdir}/stderr
